024ecb3df45daf87eccea8dc72e3818cedee2e14
Nedim Krupalija <nkrupalija1@etf.unsa.ba>
2024-02-22 11:20:52 +0100

    change displaying of keycard assignment

--- skyline-odyssey-keycard-management/ViewModels/EmployeeDetailsViewModel.cs
+++ skyline-odyssey-keycard-management/ViewModels/EmployeeDetailsViewModel.cs
@@ -21,13 +21,44 @@ namespace skyline_odyssey_keycard_management.ViewModels
         public Keycard Keycard => _selectedEmployeeStore.SelectedEmployee?.Keycard ?? new Keycard();
 
        public ICollection<UsageHistory> UsageHistories => (_selectedEmployeeStore.SelectedEmployee?.UsageHistories?? new ObservableCollection<UsageHistory>());
-        
 
+        private string _keycardIsAssigned;
+
+        public string KeycardIsAssigned
+        {
+			get
+            {
+				if (_selectedEmployeeStore.SelectedEmployee?.Keycard.IsAssigned == true)
+                {
+					_keycardIsAssigned = "Yes";
+				}
+				else
+                {
+					_keycardIsAssigned = "No";
+				}
+				return _keycardIsAssigned;
+			}
+			set
+            {
+				_keycardIsAssigned = value;
+				OnPropertyChanged(nameof(KeycardIsAssigned));
+			}
+		}
+        
         public EmployeeDetailsViewModel(SelectedEmployeeStore selectedEmployeeStore)
         {
             _selectedEmployeeStore = selectedEmployeeStore;
 
             _selectedEmployeeStore.SelectedEmployeeChanged += SelectedEmployeeStore_SelectedEmployeeChanged;
+
+            if(_selectedEmployeeStore.SelectedEmployee?.Keycard.IsAssigned == false)
+            {
+                KeycardIsAssigned = "No";
+            }    
+            else
+            {
+				KeycardIsAssigned = "Yes";
+			}
         }
 
         protected override void Dispose()
@@ -43,7 +74,7 @@ namespace skyline_odyssey_keycard_management.ViewModels
             OnPropertyChanged(nameof(Username));
             OnPropertyChanged(nameof(Role));
             OnPropertyChanged(nameof(Keycard));
-
+            OnPropertyChanged(nameof(KeycardIsAssigned));
             
             OnPropertyChanged(nameof(UsageHistories));
             
