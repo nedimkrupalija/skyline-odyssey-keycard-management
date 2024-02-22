4a8870eecd9fb73b87cd7338296df48fdd6f5be9
apasalic10 <apasalic1@etf.unsa.ba>
2024-02-22 23:39:07 +0100

    reports

--- skyline-odyssey-keycard-management/ViewModels/ReportsViewModel.cs
+++ skyline-odyssey-keycard-management/ViewModels/ReportsViewModel.cs
@@ -1,33 +1,189 @@
-﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.Collections.ObjectModel;
-using System.ComponentModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Windows;
+using Microsoft.EntityFrameworkCore;
+using skyline_odyssey_keycard_management.Models;
+using skyline_odyssey_keycard_management.Commands;
+using System.Diagnostics;
+using Microsoft.IdentityModel.Tokens;
 
 namespace skyline_odyssey_keycard_management.ViewModels
 {
     public class ReportsViewModel : ViewModelBase
     {
-        private ObservableCollection<UsageReportsListingItemViewModel> _usageReportsListingItemViewModels;
+        public List<int> Hours { get; } = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 00 };
+        private ICollection<UsageHistory> _usageReportsListingItemViewModels;
+        private DateTime _startDate;
+        private DateTime _endDate;
+        private string _selectedStartHour;
+        private string _selectedEndHour;
+        private string _nameFilter;
 
-        //public IEnumerable<UsageReportsListingItemViewModel> UsageReportsListingItemViewModels => _usageReportsListingItemViewModels;
+        private string _nameOfUser;
 
-        private DatabaseContext _databaseContext = new DatabaseContext();
-
-        public ReportsViewModel()
-        {
-            var usagereports = _databaseContext.UsageHistories.Include(u => u.AccessPoint).Include(u => u.Keycard).ToList();
-            _usageReportsListingItemViewModels = new ObservableCollection<UsageReportsListingItemViewModel>();
-            foreach (var usagereport in usagereports)
+        public string NameOfUser { 
+            get 
             {
-                _usageReportsListingItemViewModels.Add(new UsageReportsListingItemViewModel(usagereport));
-
+                return _nameOfUser;  
+            }
+            set
+            {
+                if (_nameOfUser != value)
+                {
+                    _nameOfUser = value;
+                    OnPropertyChanged(nameof(NameOfUser));
+                }
+            }
+        }
+        public DateTime StartDate
+        {
+            get { return _startDate; }
+            set
+            {
+                if (_startDate != value)
+                {
+                    _startDate = value;
+                    OnPropertyChanged(nameof(StartDate));
+                }
             }
         }
 
+        public DateTime EndDate
+        {
+            get { return _endDate; }
+            set
+            {
+                if (_endDate != value)
+                {
+                    _endDate = value;
+                    OnPropertyChanged(nameof(EndDate));
+                }
+            }
+        }
 
+        public string SelectedStartHour
+        {
+            get { return _selectedStartHour; }
+            set
+            {
+                if (_selectedStartHour != value)
+                {
+                    _selectedStartHour = value;
+                    OnPropertyChanged(nameof(SelectedStartHour));
+                }
+            }
+        }
+
+        public string SelectedEndHour
+        {
+            get { return _selectedEndHour; }
+            set
+            {
+                if (_selectedEndHour != value)
+                {
+                    _selectedEndHour = value;
+                    OnPropertyChanged(nameof(SelectedEndHour));
+                }
+            }
+        }
+
+        public string NameFilter
+        {
+            get { return _nameFilter; }
+            set
+            {
+                if (_nameFilter != value)
+                {
+                    _nameFilter = value;
+                    OnPropertyChanged(nameof(NameFilter));
+                }
+            }
+        }
+
+        public ICollection<UsageHistory> UsageHistoryList
+        {
+            get { return _usageReportsListingItemViewModels; }
+            set
+            {
+                if (_usageReportsListingItemViewModels != value)
+                {
+                    _usageReportsListingItemViewModels = value;
+                    OnPropertyChanged(nameof(UsageHistoryList));
+                }
+            }
+        }
+
+        public ReportsViewModel()
+        {
+            StartDate = DateTime.Now;
+            EndDate = DateTime.Now;
+            SelectedStartHour = "7";
+            SelectedEndHour = "18";
+            DatabaseContext db = new DatabaseContext();
+            UsageHistoryList = db.UsageHistories.Include(u=>u.AccessPoint).Include(u=>u.Keycard).ThenInclude(u=>u.User).Where(u=> (SelectedStartHour == null || u.Timestamp.Hour >= int.Parse(SelectedStartHour))
+                                    && (SelectedEndHour == null || u.Timestamp.Hour < int.Parse(SelectedEndHour))).ToList();
+        }
+        public ICommand GenerateReportCommand => new RelayCommand(GenerateReport,CanGenerateReport);
+
+        private void GenerateReport(object? parameter)
+        {
+            try
+            {
+                using (var dbContext = new DatabaseContext())
+                {
+                    string firstName;
+                    string lastName;
+
+                    if (NameFilter.Contains(" "))
+                    {
+                        var filters = NameFilter.Split(' ');
+                        firstName = filters[0];
+                        lastName = filters[1];  
+
+                    }
+                    else
+                    {
+                        firstName = NameFilter;
+                        lastName = NameFilter;
+                    }
+
+                    Trace.WriteLine(firstName + " " + lastName);
+                    var filteredReports = dbContext.UsageHistories.Include(s=>s.AccessPoint).Include(u => u.User)
+                        .Where(u => u.Timestamp.Date >= StartDate.Date
+                                    && u.Timestamp.Date <= EndDate.Date
+                                    && (SelectedStartHour == null || u.Timestamp.Hour >= int.Parse(SelectedStartHour))
+                                    && (SelectedEndHour == null || u.Timestamp.Hour < int.Parse(SelectedEndHour))
+                                    )
+                        .ToList();
+
+                    if (!NameFilter.IsNullOrEmpty())
+                    {
+                       foreach (var filter in filteredReports)
+                        {
+                            var filteredName = filter.User.FirstName + " " + filter.User.LastName;
+                            if(!filteredName.Contains(NameFilter))
+                            {
+                                filteredReports.Remove(filter);
+                            }
+                        }
+                    }
+
+                    UsageHistoryList = filteredReports;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error generating report: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+            
+        }
+
+        private bool CanGenerateReport(object? sender)
+        {
+            return true;
+        }
     }
 }
