f84bb16852ea83913fa7aaa031d610ef863ca791
Ilhan Hasicic <ilhanhasicic@gmail.com>
2024-02-22 15:18:49 +0100

    add charts to main view

--- skyline-odyssey-keycard-management/Views/MainAdminView.xaml.cs
+++ skyline-odyssey-keycard-management/Views/MainAdminView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using skyline_odyssey_keycard_management.Models;
+﻿using Microsoft.EntityFrameworkCore;
+using skyline_odyssey_keycard_management.Models;
 using skyline_odyssey_keycard_management.ViewModels;
 using System;
 using System.Collections.Generic;
@@ -31,6 +32,7 @@ namespace skyline_odyssey_keycard_management.Views
             var viewModel = new MainAdminViewModel();
             this.DataContext = viewModel;
             _databaseContext = new DatabaseContext();
+            LoadColumnChartData();
         }
 
 
@@ -66,8 +68,8 @@ namespace skyline_odyssey_keycard_management.Views
 
             LoginView loginView = new LoginView();
             LoginView.LoggedInUser.IsOnline = false;
-
-            LoginView.LoggedInUser.UsageHistories.Add(new UsageHistory(LoginView.LoggedInUser.Keycard.Id, DateTime.Now, 5,"Out")); 
+            Trace.WriteLine(LoginView.LoggedInUser);
+            LoginView.LoggedInUser.UsageHistories.Add(new UsageHistory(LoginView.LoggedInUser.Keycard.Id, DateTime.Now, 5, "Out"));
 
             _databaseContext.Update(LoginView.LoggedInUser);
 
@@ -75,17 +77,44 @@ namespace skyline_odyssey_keycard_management.Views
             this.Content = loginView;
         }
 
-        public void LoadColumnChartData()
+        private void LoadColumnChartData()
         {
+            DatabaseContext dbcontext = new DatabaseContext();
+            DateTime currentDate = DateTime.Now;
+
+            DateTime previousMonday = currentDate.AddDays(-(int)currentDate.DayOfWeek + (int)DayOfWeek.Monday - 7);
+            DateTime previousTuesday = currentDate.AddDays(-(int)currentDate.DayOfWeek + (int)DayOfWeek.Monday - 6);
+            DateTime previousWednesday = currentDate.AddDays(-(int)currentDate.DayOfWeek + (int)DayOfWeek.Monday - 5);
+            DateTime previousThursday = currentDate.AddDays(-(int)currentDate.DayOfWeek + (int)DayOfWeek.Monday - 4);
+            DateTime previousFriday = currentDate.AddDays(-(int)currentDate.DayOfWeek + (int)DayOfWeek.Monday - 3);
+
+
+            var mondayCount = dbcontext.UsageHistories.Include(x => x.AccessPoint)
+                .Where(e => e.AccessPoint.Name.Equals("Main enterance") && e.Timestamp.Day.ToString() == previousMonday.Day.ToString())
+                .ToList();
+            var tuesdayCount = dbcontext.UsageHistories.Include(x => x.AccessPoint)
+                .Where(e => e.AccessPoint.Name.Equals("Main enterance") && e.Timestamp.Day.ToString() == previousTuesday.Day.ToString())
+                .ToList();
+            var wednesdayCount = dbcontext.UsageHistories.Include(x => x.AccessPoint)
+                .Where(e => e.AccessPoint.Name.Equals("Main enterance") && e.Timestamp.Day.ToString() == previousWednesday.Day.ToString())
+                .ToList();
+            var thursdayCount = dbcontext.UsageHistories.Include(x => x.AccessPoint)
+                .Where(e => e.AccessPoint.Name.Equals("Main enterance") && e.Timestamp.Day.ToString() == previousThursday.Day.ToString())
+                .ToList();
+            var fridayCount = dbcontext.UsageHistories.Include(x => x.AccessPoint)
+                .Where(e => e.AccessPoint.Name.Equals("Main enterance") && e.Timestamp.Day.ToString() == previousFriday.Day.ToString())
+                .ToList();
+        
             ((ColumnSeries)columnChart.Series[0]).ItemsSource = new ChartDataViewModel[]
-    {
-        new ChartDataViewModel { Key = "Project Manager", Value = 12 },
-        new ChartDataViewModel { Key = "CEO", Value = 25 },
-        new ChartDataViewModel { Key = "Software Engg.", Value = 5 },
-        new ChartDataViewModel { Key = "Team Leader", Value = 6 },
-        new ChartDataViewModel { Key = "Project Leader", Value = 10 },
-        new ChartDataViewModel { Key = "Developer", Value = 4 }
-    };
+            {
+                new ChartDataViewModel { Key = "Mon", Value = mondayCount.Count},
+                new ChartDataViewModel { Key = "Tue", Value = tuesdayCount.Count },
+                new ChartDataViewModel { Key = "Wed", Value = wednesdayCount.Count },
+                new ChartDataViewModel { Key = "Thu", Value = thursdayCount.Count },
+                new ChartDataViewModel { Key = "Fri", Value = fridayCount.Count },
+            };
         }
+        
+
     }
 }
