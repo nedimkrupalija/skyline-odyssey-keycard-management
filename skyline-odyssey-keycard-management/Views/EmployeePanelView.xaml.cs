497c0adad624fd334c353b3e95f97a86119f643d
apasalic10 <apasalic1@etf.unsa.ba>
2024-02-23 00:58:21 +0100

    added usage history for logged user

--- skyline-odyssey-keycard-management/Views/EmployeePanelView.xaml.cs
+++ skyline-odyssey-keycard-management/Views/EmployeePanelView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using skyline_odyssey_keycard_management.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,7 @@ namespace skyline_odyssey_keycard_management.Views
         public EmployeePanelView()
         {
             InitializeComponent();
+            this.DataContext = new EmployeePanelViewModel();
         }
     }
 }
