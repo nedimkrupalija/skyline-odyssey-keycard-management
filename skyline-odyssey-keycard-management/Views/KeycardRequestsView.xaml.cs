e2165ed3060c09fc57dd40119277a2ae69bda133
apasalic10 <apasalic1@etf.unsa.ba>
2024-02-23 12:58:15 +0100

    finished aproved and deny requests

--- skyline-odyssey-keycard-management/Views/KeycardRequestsView.xaml.cs
+++ skyline-odyssey-keycard-management/Views/KeycardRequestsView.xaml.cs
@@ -1,6 +1,9 @@
-﻿using skyline_odyssey_keycard_management.ViewModels;
+﻿using Microsoft.EntityFrameworkCore;
+using skyline_odyssey_keycard_management.Models;
+using skyline_odyssey_keycard_management.ViewModels;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -13,6 +16,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Window;
 
 namespace skyline_odyssey_keycard_management.Views
 {
@@ -34,6 +39,63 @@ namespace skyline_odyssey_keycard_management.Views
             mainAdminView.Height = this.Height;
 
             this.Content = mainAdminView;
+        }
+
+        private void Approve_Clicked(object sender, RoutedEventArgs e)
+        {
+            if(roomListView.SelectedItem != null)
+            {
+                var selectedKeycardRequest = (KeycardRequests)roomListView.SelectedItem;
+                selectedKeycardRequest.Status = "Approved";
+                var selectedUser = selectedKeycardRequest.User;
+              
+
+                DatabaseContext databaseContext = new DatabaseContext();
+                var unassignedKeycards = databaseContext.Keycards.Where(u=>u.IsAssigned == false).ToList();
+                var selectedUserKeycard = databaseContext.Keycards.First(u => u.Id == selectedUser.KeycardId);
+
+                selectedUserKeycard.IsAssigned = false;
+                selectedUserKeycard.IsActive = true;
+                
+                selectedUser.Keycard = unassignedKeycards[0];
+                unassignedKeycards[0].IsAssigned = true;
+                unassignedKeycards[0].IsActive = true;
+
+                databaseContext.Update(selectedUserKeycard);
+                databaseContext.Update(selectedKeycardRequest);
+                databaseContext.Update(selectedUser);
+                databaseContext.Update(unassignedKeycards[0]);
+
+                databaseContext.SaveChanges();
+                roomListView.ItemsSource = databaseContext.KeycardRequests.Include(u => u.User).ToList();
+
+                string messageBoxText = "Keycard request successfully approved";
+                string _caption = "Keycard appprovel";
+                MessageBoxButton button = MessageBoxButton.OK;
+                MessageBoxImage icon = MessageBoxImage.Information;
+                MessageBox.Show(messageBoxText, _caption, button, icon, MessageBoxResult.OK);
+
+            }
+        }
+
+        private void Deny_Clicked(object sender, RoutedEventArgs e)
+        {
+            var selectedKeycardRequest = (KeycardRequests)roomListView.SelectedItem;
+            selectedKeycardRequest.Status = "Unapproved";
+            var selectedUser = selectedKeycardRequest.User;
+
+            DatabaseContext databaseContext = new DatabaseContext();
+
+            databaseContext.Update(selectedKeycardRequest);
+
+            databaseContext.SaveChanges();
+            roomListView.ItemsSource = databaseContext.KeycardRequests.Include(u => u.User).ToList();
+
+            string messageBoxText = "Keycard request successfully denied";
+            string _caption = "Keycard denied";
+            MessageBoxButton button = MessageBoxButton.OK;
+            MessageBoxImage icon = MessageBoxImage.Information;
+            MessageBox.Show(messageBoxText, _caption, button, icon, MessageBoxResult.OK);
         }
     }
 }
